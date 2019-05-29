1cdcbda7e86fdb1391ffed62313b797f4ba5f2d0
jcnimble <jcginelli@outlook.com>
2019-05-28 21:27:34 -0400

    Updates

---
+++ precache-manifest.d948dcff4cb5bfa3c26bfc037c151ca8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52c840a977bd1780dae12b0f3b2e0acc",
+    "url": "/mario-clicky/index.html"
+  },
+  {
+    "revision": "01899fb5cc44712068d4",
+    "url": "/mario-clicky/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/mario-clicky/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "01899fb5cc44712068d4",
+    "url": "/mario-clicky/static/js/main.c6df27ef.chunk.js"
+  },
+  {
+    "revision": "6f17ebb4d216980d0ffd",
+    "url": "/mario-clicky/static/js/runtime~main.2b86891e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mario-clicky/static/media/logo.5d5d9eef.svg"
+  }
+]);
